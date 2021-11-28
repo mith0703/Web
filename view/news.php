30b2e55cbf9b37bb3a88e505ac629fc995cf7e35
tri218138 <80521604+tri218138@users.noreply.github.com>
2021-11-28 18:48:31 +0700

    Update news.php
    Thay đổi nội dung đúng thực là trang tin tức

--- view/news.php
+++ view/news.php
@@ -3,117 +3,77 @@
 ?>
  
 
-<div class="container">
-            <h2 class="heading-39291 mb-0">Tin tức</h2>
+  <div class="owl-carousel-wrapper">
+        <div class="box-92819 shadow-lg">
+            <div>
+              <h1 class=" mb-3 text-black">Tin tức</h1>
+              <p>Chia sẻ những bí quyết trang trí nội thất, sự kiện, thông tin và dự án từ Diamond</p>
+            </div>
+        </div>
+        <div class="ftco-cover-1" style="background-image: url('./libs/images/hero_1.jpg');"></div>
+      </div>
+      <div class="site-section">
+        <div class="container"> 
             <div class="d-flex flex-sm-row flex-column flex-wrap container-md">
               <div class="col-md-6 col-12">
-                <div class="product">
-    
-                  <img src="./libs/images/bacau.png" />
-    
-    
-    
+                <div class="product">    
+                  <img src="./libs/images/img_1.jpg" /> 
                   <div class="intro">
                     <p>
-                      <strong>Bắc Âu </strong>nền trang trí hiện đại theo khuynh hướng
-                      thế giới, lịch sử, tao nhã và trang trọng
-                    </p>
-    
-                  </div class="summary">
-    
-                  <div class="text">
-                    THIẾT KẾ NỘI THẤT BIỆT THỰ DƯƠNG NỘI
-                    <br>
-                    Chủ đề thiết kế: Không gian xanh
-                    <br>
-                    Diện tích: 50m2
-                    <div class="active-scroll">
-                      <input class="modal-btn" type="checkbox" id="modal-btn" name="modal-btn" />
-                      <label for="modal-btn">Thêm thông tin <i class="uil uil-expand-arrows"></i></label>
-                      <div class="modal">
-                        <div class="modal-wrap">
-                          <img class="inside-wrap" src="./libs/images/bacau.png" alt="product1">
-                          <p>THIẾT KẾ NỘI THẤT BIỆT THỰ DƯƠNG NỘI
-                            <br>
-                            Chủ đề thiết kế: Không gian xanh
-                            <br>
-                            Diện tích: 50m2
-                            <br>
-                            Màu chủ đạo: Xanh lá
-                            <br>
-                            Công nghệ đặc sắc: in 3D, gỗ hiếm
-                            <br>
-                            Giá: 12.000.000đ
-                          </p>
-                          <p>
-                            Conment
-                          <div class="fb-comments" data-href="http://127.0.0.1:5500/Products2.html" data-width="800"
-                            data-numposts="5"></div>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
+                      <a href="#">LÀM THẾ NÀO ĐỂ MỞ RỘNG KHÔNG GIAN BẰNG GIẢI PHÁP ÁNH SÁNG?</a>
+                    </p>    
+                  </div>
+                </div> 
+              </div>   
+              <div class="col-md-6 col-12">
+                <div class="product">                
+                  <img src="./libs/images/img_2.jpg" />
+                  <div class="intro">
+                    <p>
+                      <a href="#">TÌM HIỂU VỀ 7 KIỂU CẦU THANG PHỔ BIẾN NHẤT</a>
+                    </p>    
                   </div>
                 </div>
-    
               </div>
-    
+              <div class="col-md-6 col-12">
+                <div class="product">    
+                  <img src="./libs/images/img_3.jpg" />    
+                    <div class="intro">
+                      <p>
+                        <a href="#">NGHỆ THUẬT ÁNH SÁNG TRONG KHÔNG GIAN TRƯNG BÀY</a>
+                      </p>    
+                    </div>
+                </div>
+              </div>
               <div class="col-md-6 col-12">
                 <div class="product">
-    
-                  <img src="./libs/images/bacau.png" />
-    
-    
-    
+                  <img src="./libs/images/img_4.jpg" />
                   <div class="intro">
                     <p>
-                      <strong>Bắc Âu </strong>nền trang trí hiện đại theo khuynh hướng
-                      thế giới, lịch sử, tao nhã và trang trọng
+                      <a href="#">5 ĐIỀU BẠN CÓ THỂ LÀM ĐỂ CÓ THỂ NÂNG CẤP KHÔNG GIAN LÀM VIỆC TẠI NHÀ</a>
                     </p>
-    
-                  </div class="summary">
-    
-                  <div class="text">
-                    THIẾT KẾ NỘI THẤT BIỆT THỰ DƯƠNG NỘI
-                    <br>
-                    Chủ đề thiết kế: Không gian xanh
-                    <br>
-                    Diện tích: 50m2
-                    <div class="active-scroll">
-                      <input class="modal-btn" type="checkbox" id="modal-btn" name="modal-btn" />
-                      <label for="modal-btn">Thêm thông tin <i class="uil uil-expand-arrows"></i></label>
-                      <div class="modal">
-                        <div class="modal-wrap">
-                          <img class="inside-wrap" src="./libs/images/bacau.png" alt="product1">
-                          <p>THIẾT KẾ NỘI THẤT BIỆT THỰ DƯƠNG NỘI
-                            <br>
-                            Chủ đề thiết kế: Không gian xanh
-                            <br>
-                            Diện tích: 50m2
-                            <br>
-                            Màu chủ đạo: Xanh lá
-                            <br>
-                            Công nghệ đặc sắc: in 3D, gỗ hiếm
-                            <br>
-                            Giá: 12.000.000đ
-                          </p>
-                          <p>
-                            Conment
-                          <div class="fb-comments" data-href="http://127.0.0.1:5500/Products2.html" data-width="800"
-                            data-numposts="5"></div>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
                   </div>
                 </div>
-    
               </div>
-    
             </div>
-         
+          </div>
         </div>
-      </div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+          </li>
+
+          <li class="page-item active">
+            <a class="page-link" href="#">1<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
 
       <?php
   require_once("./view/footer.php");
