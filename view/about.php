4e9c2c2af7fd1f31a341dfffe8afee04d982ddc9
tri218138 <80521604+tri218138@users.noreply.github.com>
2021-11-28 22:48:31 +0700

    Add files via upload

---
+++ view/about.php
@@ -0,0 +1,176 @@
+<?php
+  require_once("./view/header.php");
+?>
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+    <div class="owl-carousel-wrapper">
+      <div class="box-92819 shadow-lg">
+          <div>
+            <h1 class=" mb-3 text-black">Giới thiệu</h1>
+            <p>Tìm hiểu những yếu tố tạo nên sự khác biệt của Diamond.            </p>
+          </div>
+        
+      </div>
+
+        <div class="ftco-cover-1" style="background-image: url('./libs/images/hero_1.jpg');"></div>
+    </div>
+
+    
+    <div class="site-section">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-lg-4">
+            <div class="h-100 bg-white box-29291">
+              <h2 class="heading-39291">Lịch sử công ty</h2>
+<p>Tiền thân là một công ty thiết kế và thi công có bề dày hoạt động gần 15 năm, với đội ngũ gồm nhiều chuyên gia giàu kinh nghiệm trong lãnh vực kiến trúc, thiết kế và xây dựng, bên cạnh lực lượng công nhân kỹ thuật tận tuỵ, có tay nghề cao. Từ công trình văn phòng làm việc, cửa hàng kinh doanh, nhà hàng khách sạn, căn hộ cao cấp cho đến nhà ở tư nhân, Diamond cam kết hướng đến không gian sinh hoạt hiện đại, thân thiện, giản tiện, phù hợp với chức năng, chi phí và phong cách của từng đối tượng khách hàng.</p>
+
+             
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="owl-carousel owl-3">
+              <img src="./libs/images/about_1.jpg" alt="Image" class="img-fluid">
+              <img src="./libs/images/about_2.jpg" alt="Image" class="img-fluid">
+              <img src="./libs/images/about_3.jpg" alt="Image" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5 section-2-title">
+          <div class="col-md-6">
+            <h2 class="heading-39291">Team của chúng tôi</h2>
+                 </div>
+        </div>
+        <div class="row align-items-stretch">
+
+          
+          <div class="col-lg-4 col-md-6 mb-5">
+            <div class="post-entry-1 h-100 bg-white text-center">
+              <a href="#" class="d-inline-block">
+                <img style="width: 200px;" src="./libs/images/person_2.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                <span class="meta">CEO and Co-Founder</span>
+                <h2>Ngô Phúc Thành</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-5">
+            <div class="post-entry-1 h-100 bg-white text-center">
+              <a href="#" class="d-inline-block">
+                <img style="width: 200px;" src="https://scontent.fsgn5-11.fna.fbcdn.net/v/t39.30808-1/p320x320/214197081_1239854566472833_6124847357724474649_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7206a8&_nc_ohc=efX1J4OxLRUAX-UJLiT&_nc_ht=scontent.fsgn5-11.fna&oh=bb80e3d96563d07b0a7330fc74eea67b&oe=61A7EE15" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                <span class="meta">CEO and Co-Founder</span>
+                <h2>Đoàn Trần Cao Trí</h2>
+  
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-5">
+            <div class="post-entry-1 h-100 bg-white text-center">
+              <a href="#" class="d-inline-block">
+                <img style="width: 200px;" src="./libs/images/person_3.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                <span class="meta">CEO and Co-Founder</span>
+                <h2>Lê Nhựt Anh</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-5">
+            <div class="post-entry-1 h-100 bg-white text-center">
+              <a href="#" class="d-inline-block">
+                <img style="width: 200px;" src="./libs/images/person_4.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                <span class="meta">Giám Đốc Thi Công</span>
+                <h2>Diamond Team</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-5">
+            <div class="post-entry-1 h-100 bg-white text-center">
+              <a href="#" class="d-inline-block">
+                <img style="width: 200px;" src="./libs/images/person_5.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                <span class="meta">Giám Đốc Marketing</span>
+                <h2>Diamond Team</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-5">
+            <div class="post-entry-1 h-100 bg-white text-center">
+              <a href="#" class="d-inline-block">
+                <img style="width: 200px;" src="./libs/images/person_1.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                <span class="meta">Giám Đốc Sáng Tạo</span>
+                <h2>Diamond Team</h2>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+
+    
+    <div class="site-section section-4">
+      <div class="container">
+
+        <div class="row justify-content-center text-center">
+          <div class="col-md-7">
+            <div class="slide-one-item owl-carousel">
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>Đội ngũ kỹ thuật, thiết kế và thi công của Diamond đã làm việc sát cánh với chúng tôi ngay từ thiết kế sơ phát, trong suốt quá trình thi công và thậm chí ngay sau khi bàn giao để vận hành.</p>
+                <cite><span class="text-black">Ngô Phúc Thành</span> &mdash; <span class="text-muted">CEO and Co-Founder</span></cite>
+              </blockquote>
+
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>Có thể nói, Diamond sở hữu được mức độ hài lòng cao nhất đối với các dự án mà chúng tôi uỷ thác triển khai, xét về tiến độ lẫn chất lượng thi công.</p>
+                <cite><span class="text-black">Đoàn Trần Cao Trí</span> &mdash; <span class="text-muted">CEO and Co-Founder</span></cite>
+              </blockquote>
+
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>“Tận tình” và “luôn nghĩ cho khách hàng” chính là hai điều đặc biệt nhất của Diamond</p>
+                <cite><span class="text-black">Lê Nhựt Anh</span> &mdash; <span class="text-muted">CEO and Co-Founder</span></cite>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php
+  require_once("./view/footer.php");
+?>
