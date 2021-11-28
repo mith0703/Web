8c98f332af7f650e122e545552d7f5978165fbd9
tri218138 <80521604+tri218138@users.noreply.github.com>
2021-11-28 18:34:18 +0700

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,99 @@
+<footer>
+<div class="d-flex p-2 flex-column flex-wrap align-content-center" style="background-color:#f8f9fa;">
+        <div class="d-flex p-2 flex-md-row flex-column gap-3 flex-wrap align-content-center">
+            <div style="padding-right: 1em;">
+                <!-- <image src="images/logo-footer.png" rounded  style="margin-left:-65%; margin-right:auto;" /> -->
+                <br/>
+                <p style="color:#FF9900; text-align:left;">CÔNG TY CPĐT & THIẾT KẾ NỘI THẤT DIAMOND</p>
+                <div style="color:black; text-align:left;"> 
+                <i class="fa fa-map-marker me-2" aria-hidden="true"></i>
+                <p><strong>Trụ sở văn phòng:</strong> Trường Đại học Bách khoa - ĐHQG TP.HCM</p>
+            
+                </div>
+                <div style="color:black; text-align:left;"> 
+                <i class="fa fa-map-marker me-2" aria-hidden="true"></i>
+                <p><strong>Thời gian làm việc: </strong> 8h – 18h kể cả thứ 7</p>  
+                </div>
+                <div style="color:black; text-align:left;"> 
+                <i class="fa fa-phone me-2" aria-hidden="true"></i>
+                    <p><strong>Số điện thoại liên lạc: </strong>0987654321</p>  
+
+                </div>
+                <div style="color:black; text-align:left;"> 
+                <i class="fa fa-envelope-o me-2" aria-hidden="true"></i>
+                    
+                    <p><strong>Thư điện tử: </strong>noithatdiamondk20@gmail.com</p>  
+
+                </div>
+                <div style="color:black; text-align:left;"> 
+                <i class="fa fa-map me-2" aria-hidden="true"></i>
+                    <a href="https://www.google.com/maps/place/Tr%C6%B0%E1%BB%9Dng+%C4%91%E1%BA%A1i+h%E1%BB%8Dc+B%C3%A1ch+Khoa/@10.879892,106.806315,14z/data=!4m5!3m4!1s0x0:0x913146948e01ee20!8m2!3d10.879892!4d106.8063146?hl=en"><h3>Địa chỉ trên Google Map</h3>  </a>
+                </div>
+                  
+            </div>
+            <div style="padding-right: 1em;"><br/>
+                      <h5 style="color:black;">CHÍNH SÁCH</h5>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left ; margin-left:2px;">Hình thức thanh toán</h6>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left;">Bảo mật thông tin</h6>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left;">Vận chuyển giao nhận</h6>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left;">Bảo hành - Đổi trả</h6>
+                  </div>
+                  <div style="padding-right: 1em;"><br/>
+                  <h5 style="color:black;">TIN TỨC</h5>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left ; margin-left:2px;">Hình thức thanh toán</h6>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left;">Bảo mật thông tin</h6>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left;">Vận chuyển giao nhận</h6>
+                      <hr style="color:black;"/>
+                      <h6 style="color:black; text-align:left;">Bảo hành - Đổi trả</h6>
+                  </div>
+                  <div ><br/>
+                      <h5 style="color:black;">THEO DÕI CHÚNG TÔI</h5>
+                      <img height="50px" weight="50px" src="libs/images/youtube.png" onclick="targetImage('https:\\www.youtube.com/feed/trending')" style="cursor:pointer;" />
+                      <img height="50px" weight="50px" src="libs/images/zalo_sharelogo.png"onclick="targetImage('https:\\zalo.me/pc')" style="cursor:pointer;" />
+                      <img height="50px" weight="50px" src="libs/images/fb-logo.png"onclick="targetImage('https:\\www.facebook.com/')" style="cursor:pointer;" />
+                      <br/>
+                      <br/>
+                        <script>
+                            function targetImage(value){
+                                window.open(value, '_blank');
+                            }
+                        </script>
+      
+                  </div>
+              </div>
+              <div>
+              <hr style="color:black; width:100%;"/>
+               <p  style="color:black;">   Nội thất Diamond</p>
+              </div>
+          </div>
+</footer>
+    
+    
+
+    </div>
+
+    <script src="/mvc/libs/js/jquery-3.3.1.min.js"></script>
+    <script src="/mvc/libs/js/popper.min.js"></script>
+    <script src="/mvc/libs/js/bootstrap.min.js"></script>
+    <script src="/mvc/libs/js/owl.carousel.min.js"></script>
+    <script src="/mvc/libs/js/jquery.sticky.js"></script>
+    <script src="/mvc/libs/js/jquery.waypoints.min.js"></script>
+    <script src="/mvc/libs/js/jquery.animateNumber.min.js"></script>
+    <script src="/mvc/libs/js/jquery.fancybox.min.js"></script>
+    <script src="/mvc/libs/js/jquery.easing.1.3.js"></script>
+    <script src="/mvc/libs/js/aos.js"></script>
+
+    <script src="/mvc/libs/js/main.js"></script>
+   <script src="/mvc/libs/js/private.js"></script>
+     
+
+  </body>
+
+</html>
