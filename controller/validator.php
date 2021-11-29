0049de53b686776d3864b9110a05b8850d13bf0e
nhut anh <nhut.anh2501@gmail.com>
2021-11-29 12:41:08 +0700

    validator

--- controller/validator.php
+++ controller/validator.php
@@ -10,4 +10,25 @@ function isNameValid($name){
 
 }
 
+function isPasswordValid($name){
+    $exp = "/[a-zA-Z0-9]{5,40}/";
+    if (!preg_match($exp, $name)){
+        echo "Vui lòng nhâp chuỗi từ 5-40 ký tự \n";
+    } else {
+        return $name;
+    }
+
+}
+
+function isEmailValid ($email) {
+    
+    $regex = "/[a-zA-z0-9]@[a-zA-z0-9].[a-zA-z0-9]/i";
+    if (preg_match( $regex, $email)) {
+    $em = 1;
+    } else {
+    echo "Email - Vui long nhap < sth >@< sth >.< sth ><br>";
+    $em = 0;
+    }
+};
+
 ?>
